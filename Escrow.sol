e68f774a9be05cc23b4032fc8faa29cfbe1ef5b5
nick-lazzari <113952846+nick-lazzari@users.noreply.github.com>
2022-11-21 12:01:45 -0500

    Add files via upload

---
+++ Escrow.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
+
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can do this");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can do this");
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Only inspector can do this");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed; 
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
+
+    constructor(address _nftAddress, address payable _seller, address _inspector, address _lender) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function list(uint256 _nftID, address _buyer, uint256 _purchasePrice, uint256 _escrowAmount) public payable onlySeller {
+        //Transfer NFT from seller -> this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        //Updates mapping
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
+
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+     function updateInspectionStatus(uint256 _nftID, bool _passed) public onlyInspector() {
+        inspectionPassed[_nftID] = _passed;
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
+   
+
+}
